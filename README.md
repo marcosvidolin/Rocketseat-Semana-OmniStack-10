826a8710acd9eca40cb1ab579ebb9fe432e2b5fb
Marcos Vidolin <marcosvidolin@gmail.com>
2020-01-15 06:05:38 -0300

    Configurado a conexao do banco no .env

--- README.md
+++ README.md
@@ -1,11 +1,30 @@
 # RocketSeat - semana OmniStack edição 10
 
-## Run
+Projeto de estudo da semana OmniStack do RocketSeat
+
+## Set up
+
+Instale as dependencias do projeto rodando o comando
+
+```shell
+yarn install
+```
+
+Configure as variaveis de ambiente criando o arquivo ".env" na raiz do projeto com o conteudo:
+
+```shell
+DB_USERNAME=<trocar pelo seu user de banco>
+DB_PASSWORD=<trocar pelo seu password de banco>
+```
+
+## How to Run
+
+Execute o comando na raiz do projeto
 
 ```shell
 yarn dev
 ```
 
-## MongoDB
+## Database
 
-https://cloud.mongodb.com
+[MongoDB Atlas](https://www.mongodb.com/cloud/atlas)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongosse = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
 
-mongosse.connect('mongodb+srv://omnistack:omnistack@cluster0-tuars.mongodb.net/week10?retryWrites=true&w=majority', { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+const dataBaseConnection = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-tuars.mongodb.net/week10?retryWrites=true&w=majority`;
+mongosse.connect(dataBaseConnection, { 
+    useNewUrlParser: true, // Fix: DeprecationWarning: current URL string parser is deprecated
+    useUnifiedTopology: true, // Fix: DeprecationWarning: current Server Discovery and Monitoring engine is deprecated
+    useCreateIndex: true, // Fix: DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
 });
 
 app.use(express.json());
